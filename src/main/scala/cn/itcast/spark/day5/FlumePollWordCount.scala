12c1800442da5ef2e9601b8132902c849f6306ba
iathanasy <14163548@qq.com>
2018-08-09 16:03:10 +0800

    解析的数据

--- src/main/scala/cn/itcast/spark/day5/FlumePollWordCount.scala
+++ src/main/scala/cn/itcast/spark/day5/FlumePollWordCount.scala
@@ -12,7 +12,7 @@ object FlumePollWordCount {
     val conf = new SparkConf().setAppName("FlumePollWordCount").setMaster("local[2]")
     val ssc = new StreamingContext(conf, Seconds(5))
     //从flume中拉取数据(flume的地址)
-    val address = Seq(new InetSocketAddress("172.16.0.11", 8888))
+    val address = Seq(new InetSocketAddress("36.111.163.80", 8888))
     val flumeStream = FlumeUtils.createPollingStream(ssc, address, StorageLevel.MEMORY_AND_DISK)
     val words = flumeStream.flatMap(x => new String(x.event.getBody().array()).split(" ")).map((_,1))
     val results = words.reduceByKey(_+_)
